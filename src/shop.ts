ac50788d70dcd845b5301001e867470c9b7de9cd
vikaBu <vikabuklajevafoto@gmail.com>
2020-08-26 12:02:10 +0100

    test working

--- src/shop.ts
+++ src/shop.ts
@@ -3,64 +3,65 @@
     quality: number;
     sellIn: number;
 }﻿
-
 const isLegendary = (item: Item): boolean => {
     return item.name === 'Sulfuras, Hand of Ragnaros';
 }
-
 const isAgedBrie = (item: Item): boolean => {
     return item.name === 'Aged Brie';
 }
-
 const isBackstagePass = (item: Item): boolean => {
     return item.name === 'Backstage passes to a TAFKAL80ETC concert';
 }
-
 const getUpdatedSellIn = (item: Item): number => {
     if (isLegendary(item)) {
         return item.sellIn;
     }
     return item.sellIn - 1;
 }
-
-const isRegularItem = (item: Item): boolean => {
-    return !isLegendary(item) && !isAgedBrie(item) && !isBackstagePass(item)
-}
-
 const getUpdatedRegularItemQuality = (item: Item): number => {
     if (item.quality > 0) {
         return item.quality - 1;
     }
     return 0;
 }
+const getUpdatedBackstagePassQuality = (item: Item): number => {
+    let newQuality = item.quality + 1;
+    if (item.sellIn< 11) {
+        newQuality ++;
+    }
+    if (item.sellIn< 6) {
+        newQuality ++;
+    }
+    if (newQuality >= 50) {
+        newQuality = 50;
+    }
+    return newQuality;
+}
+
+const getUpdatedAgedBrieQuality = (item: Item): number => {
+    let newQuality = item.quality + 1;
+    if (item.sellIn <= 0) {
+        newQuality++;
+    }
+    if (newQuality >= 50) {
+        newQuality = 50;
+    }
+    return newQuality;
+}
 
 const getUpdatedQuality = (item: Item): number => {
-    if (isRegularItem(item)) {
-        return getUpdatedRegularItemQuality(item);
-    }
     
     if (isLegendary(item)) {
         return item.quality;
     }
-    
-    if (item.quality < 50) {
-        item.quality = item.quality + 1;
-        if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.sellIn < 11) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
-            if (item.sellIn < 6) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
-        }
+    if (isAgedBrie(item)) {
+        return getUpdatedAgedBrieQuality(item);
     }
-    return item.quality;
+    if (isBackstagePass(item)) {
+        return getUpdatedBackstagePassQuality(item);
+    }
+    return getUpdatedRegularItemQuality(item);
 }
-
 export const updateQuality = (items: Item[]): Item[] => {
     items.forEach((item: Item) => {
         item.quality = getUpdatedQuality(item);
@@ -76,11 +77,8 @@ export const updateQuality = (items: Item[]): Item[] => {
                 } else {
                     item.quality = item.quality - item.quality;
                 }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
             }
+            
         }
     });
 
--- tests/shop.test.ts
+++ tests/shop.test.ts
@@ -51,4 +51,47 @@ describe("The update item function", () => {
             })
         })
     })
+
+    describe("of backstage passes", () => {
+        it("by increasing it by 1 if more than 11 days before concert", () => {
+            const items = [{ name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 11, quality: 20}];
+            const updatedItems = updateQuality(items);
+            expect(updatedItems[0].quality).toBe(21);
+        });
+        it("by increasing it by 2 if 10 or less days before concert", () => {
+            const items = [{ name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 20}];
+            const updatedItems = updateQuality(items);
+            expect(updatedItems[0].quality).toBe(22);
+        });
+        it("by increasing it by 3 if 5 or less days before concert", () => {
+            const items = [{ name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 20}];
+            const updatedItems = updateQuality(items);
+            expect(updatedItems[0].quality).toBe(23);
+        });
+        it("the quality does not go above 50", () => {
+            const items = [{ name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 50}];
+            const updatedItems = updateQuality(items);
+            expect(updatedItems[0].quality).toBe(50);
+        });
+
+    })
+    describe("of Aged Brie", () => {
+        it("by increasing the quality by 1 up to 50", () => {
+            const items = [{name: "Aged Brie", sellIn: 20, quality: 20}];
+            const updatedItems = updateQuality(items);
+            expect(updatedItems[0].quality).toBe(21);
+        })
+        it("quality does not go above 50", () => {
+            const items = [{name: "Aged Brie", sellIn: 20, quality: 50}];
+            const updatedItems = updateQuality(items);
+            expect(updatedItems[0].quality).toBe(50);
+        })
+        it("if expired add 2 up to 50", () => {
+            const items = [{name: "Aged Brie", sellIn: 0, quality: 20}];
+            const updatedItems = updateQuality(items);
+            expect(updatedItems[0].quality).toBe(22);
+        })
+
+    })
 });
+
